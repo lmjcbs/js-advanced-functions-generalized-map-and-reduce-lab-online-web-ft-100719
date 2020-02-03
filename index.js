4c51681992bd0578b35266254c9d92a915ab7f36
lmjcbs <hello@liamjacobs.dev>
2020-02-03 12:54:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+const map = (array, func) => {
+  const newArray = []
+  array.forEach(element => {
+    newArray.push(func(element))  
+  });
+  return newArray
+}
+
+const reduce = (array, func, initial) => {
+  let result = !!initial ? initial : array[0];
+  let i = !!initial ? 0 : 1;
+  for (; i < array.length; i++) {
+    result = func(array[i], result)
+  }
+  return result
+}
